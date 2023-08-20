83d724b47de07f11bf4dde0819e123839e4be8e1
Ralph Lavelle <ralphlavelle@gmail.com>
2023-08-20 17:08:28 +0300

    Added Galleries list

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -1,5 +1,6 @@
 'use client'
 import { useState } from 'react';
+import styles from '../styles.module.scss'
 
 export default function SearchForm() {
 
@@ -9,9 +10,14 @@ export default function SearchForm() {
 		setEmailAddressRevealed(!emailAddressRevealed)
 	}
 
+	const email = "gavinlavelleart@gmail.com"
+
 	return (
-		<div>
-			<a href="https://twitter.com/GavinLavelle" target="_blank"><i className="fa-brands fa-x-twitter"></i></a> | <a href="https://www.flickr.com/photos/gavinlavelle" target="_blank"><i className="fa-brands fa-flickr"></i></a> | <button onClick={showEmailAddress}><i className="fa-regular fa-envelope"></i></button> { emailAddressRevealed ? <span > gavinlavelleart@gmail.com </span> : null } | Site by <a href="https://ralphlavelle.net" target="_blank">RL</a>
-		</div>
+		<>
+			{ emailAddressRevealed ? <div className={styles.emailBlock}>{ email }</div> : null }
+			<div>
+				<a href="https://twitter.com/GavinLavelle" target="_blank"><i className="fa-brands fa-x-twitter"></i></a> | <a href="https://www.flickr.com/photos/gavinlavelle" target="_blank"><i className="fa-brands fa-flickr"></i></a> | <button onClick={showEmailAddress}><i className="fa-regular fa-envelope"></i></button> | Site by <a href="https://ralphlavelle.net" target="_blank">RL</a>
+			</div>
+		</>
 	)
 }
--- app/page.tsx
+++ app/page.tsx
@@ -8,9 +8,17 @@ export default function Home() {
 
   return (
     <>
-      <div className={styles.intro}>
+      <div className={styles.intro1}>
         <h3>Irish artist, based in Clifden, Co. Galway, Ireland</h3>
-        <h3><a href="http://lavelleartgallery.ie/" target="_blank">Lavelle Art Gallery</a></h3>
+      </div>
+      <div className={styles.intro2}>
+        <h3>In galleries:</h3>
+        <ul>
+          <li><a href="http://lavelleartgallery.ie/artists/paintings/gavin-lavelle/" target="_blank">The Lavelle Art Gallery, Clifden, Co. Galway</a></li>
+          <li><a href="https://canvasgalleries.com/collections/gavin-lavelle" target="_blank">Canvas Galleries, Belfast, Northern Ireland</a></li>
+          <li><a href="https://rhagallery.ie/rha-annual" target="_blank">RHA (Royal Hibernian Academy) Annual Exhibition, Dublin</a></li>
+          <li><a href="https://www.royalulsteracademy.org/annual-exhibition/" target="_blank">Royal Ulster Academy of Arts Annual Exhibition, Belfast, Northern Ireland</a></li>
+        </ul>
       </div>
       { collections.map((c, i) => (
         <div key={c.id} className={styles.menu}>
