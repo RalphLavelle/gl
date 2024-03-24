0442bcc163741850c8ab742b276a753247d3c72f
Ralph Lavelle <ralphlavelle@gmail.com>
2024-03-25 08:46:41 +1000

    Removed X, put Insta

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -16,7 +16,7 @@ export default function Footer() {
 		<footer>
 			{ emailAddressRevealed ? <div className={styles.emailBlock}>{ email }</div> : null }
 			<div>
-				<a href="https://twitter.com/GavinLavelle" target="_blank"><i className="fa-brands fa-x-twitter"></i></a> | <a href="https://www.flickr.com/photos/gavinlavelle" target="_blank"><i className="fa-brands fa-flickr"></i></a> | <button onClick={showEmailAddress}><i className="fa-regular fa-envelope"></i></button> | <a href="https://ralphlavelle.net" target="_blank">Site by RL</a>
+				<a href="https://www.instagram.com/gavin.lavelle" target="_blank"><i className="fa-brands fa-instagram"></i></a> | <a href="https://www.flickr.com/photos/gavinlavelle" target="_blank"><i className="fa-brands fa-flickr"></i></a> | <button onClick={showEmailAddress}><i className="fa-regular fa-envelope"></i></button> | <a href="https://ralphlavelle.net" target="_blank">Site by RL</a>
 			</div>
 		</footer>
 	)
